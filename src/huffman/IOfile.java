0698e84a7066cfd19168e51657fbef010bf9d5b3
sonatalight <light.of.sonata@gmail.com>
2015-04-12 14:49:25 +0900

    coba cek

--- src/huffman/IOfile.java
+++ src/huffman/IOfile.java
@@ -5,26 +5,48 @@
  */
 package huffman;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 /**
  *
  * @author FIRMAN
  */
 public class IOfile {
-        String Url;
-        public IOfile(String Url){
         
+        private final  InputStream inputStream;
+
+    public IOfile(String Url) throws FileNotFoundException {
+        this.inputStream = new FileInputStream(Url);
+    }
+         
         
-        
-        
-        }
-    //Method untuk mendapatkan Leaf Untuk hufmman
-    public Node[] GetAllLeaf(){
-    //Dummy data
-      Node [] test = new Node[4];
-       for(int i=0 ;i<4;i++){
-       test[i]=  new Node(i+1,true,Integer.toHexString(i+1));
+       private  int[] HexStream() throws IOException{
+        long streamPtr=0;
+        int []Ptr =new int[256];
+        for(int i=0;i<256;i++){
+           Ptr[i] = 0;
+    }
+        while (inputStream.available() > 0) { 
+            System.out.print("a");
+               Ptr[inputStream.read()]++;               
+    }
+            return Ptr;
        }
-    return  test;
+
+    //Method untuk mendapatkan Leaf Untuk hufmman
+    public Node[] GetAllLeaf() throws IOException {
+        ArrayList<Node> res = new ArrayList<Node>();
+        int[] Count = HexStream();
+        for(int i=0;i<256;i++){
+           if(Count[i] != 0){
+               res.add(new Node(Count[i],true,Integer.toBinaryString(i)));
+           }
+    }
+    return  (Node[])res.toArray();
     }
     
     
